90a9d527b2a04a54d1118d4e022f62d5d8cf7f43
anikaba <bukkitjava@gmail.com>
2017-06-30 07:11:20 +0800

    怪物計劃重構 怪物計畫書GUI介面

---
+++ src/main/java/tw/org/anikaba/UserInterface/item/RunesUi.java
@@ -0,0 +1,7 @@
+package tw.org.anikaba.UserInterface.item;
+
+/**
+ * Created by jin on 2017/6/30.
+ */
+public class RunesUi {
+}
---
+++ src/main/java/tw/org/anikaba/UserInterface/monster/PlanUi.java
@@ -0,0 +1,15 @@
+package tw.org.anikaba.UserInterface.monster;
+
+import javax.swing.*;
+
+public class PlanUi {
+    private JRadioButton 新增怪物資料RadioButton;
+    private JButton 確定Button;
+    private JPanel Index;
+    private JRadioButton 讀取怪物資料RadioButton;
+    private JRadioButton 關閉怪物計畫書RadioButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
